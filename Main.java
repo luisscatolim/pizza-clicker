848103341ad5da749c03ff27699bfb5646528a94
Luis Otavio <luisscatolim@gmail.com>
2026-09-12 13:58:08 -0300

    New decriptions of code and rename the window (janela) to (window)

--- Main.java
+++ Main.java
@@ -12,43 +12,44 @@ public class Main {
     private static int contador = 0;
 
     public static void main(String[] args) {
-        JFrame janela = new JFrame("Pizza Clicker"); // titulo
-        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        janela.setSize(400, 450);
-        janela.setLocationRelativeTo(null);
-        janela.setLayout(new GridBagLayout());
+        JFrame window = new JFrame("Pizza Clicker"); // title of window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the program when close the window
+        window.setSize(400, 450); // set the size of window
+        window.setLocationRelativeTo(null); // 
+        window.setLayout(new GridBagLayout());
+        window.setResizable(false);
 
-        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagConstraints gbc = new GridBagConstraints(); // create to control the swing
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.anchor = GridBagConstraints.CENTER; // set on center
 
-        JLabel label = new JLabel("Total: 0");
-        label.setFont(new Font("Arial", Font.BOLD, 22));
-        janela.add(label, gbc);
+        JLabel label = new JLabel("Total: 0"); // create the text of points
+        label.setFont(new Font("Arial", Font.BOLD, 22)); // simple configs of text
+        window.add(label, gbc);
 
         int largura = 250;
         int altura = 250;
 
-        ImageIcon rawIcon = new ImageIcon("assets/pizza1.png");
-        Image img = rawIcon.getImage().getScaledInstance(largura, altura, Image.SCALE_SMOOTH);
-        ImageIcon iconPizza = new ImageIcon(img);
+        ImageIcon rawIcon = new ImageIcon("assets/pizza1.png"); //  load the original image
+        Image img = rawIcon.getImage().getScaledInstance(largura, altura, Image.SCALE_SMOOTH); // configure the image to create the icon
+        ImageIcon iconPizza = new ImageIcon(img); // create the icon of pizza's button
 
-        JButton pizza = new JButton(iconPizza);
-        pizza.setPreferredSize(new Dimension(largura, altura));
-        pizza.setContentAreaFilled(false);
+        JButton pizza = new JButton(iconPizza); // create the button/clicker
+        pizza.setPreferredSize(new Dimension(largura, altura)); // set the size of button
+        pizza.setContentAreaFilled(false); // set the area of button
         pizza.setBorderPainted(false);
         pizza.setFocusPainted(false);
         pizza.setOpaque(false);
 
-        pizza.addActionListener(e -> {
-            contador++;
+        pizza.addActionListener(e -> { // when click on pizza button
+            contador++; 
             label.setText("Total: " + contador);
         });
 
         gbc.gridy = 1;
-        janela.add(pizza, gbc);
+        window.add(pizza, gbc);
 
-        janela.setVisible(true);
+        window.setVisible(true);
     }
 }
