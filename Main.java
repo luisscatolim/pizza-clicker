1bd16c243ba5c306b5fb977dbd748beb0ea087e1
Luis Scatolim <luisomscatolim@gmail.com>
2026-09-14 22:03:39 -0300

    Simplifying the code to everyone understand

--- Main.java
+++ Main.java
@@ -2,54 +2,55 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.Dimension;
+import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Image;
 
 public class Main {
-    private static int numPizzas = 0; // number of clicks/pizzas
+    // Num of pizzas
+    private static int numPizzas = 0; 
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("Pizza Clicker"); // title of window
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the program when close the window
-        window.setSize(500, 500); // set the size of window
-        window.setLocationRelativeTo(null); // 
-        window.setLayout(new GridBagLayout());
-        window.setResizable(false);
+        // Config window
+        JFrame window = new JFrame("Pizza Clicker"); // Ttitle
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the program when click in X
+        window.setSize(500, 500); // size of window, x, y
+        window.setLocationRelativeTo(null); // open window in center of screen
+        window.setResizable(false); // lock the resize of window
 
-        GridBagConstraints gbc = new GridBagConstraints(); // create to control the swing
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.CENTER; // set on center
+        // config the points
+        JLabel totalText = new JLabel("Total: 0"); 
+        totalText.setFont(new Font("Arial", Font.BOLD, 28));
+        totalText.setHorizontalAlignment(JLabel.CENTER); // Centralize the text
 
-        JLabel totalText = new JLabel("Total: 0"); // create the text of points
-        totalText.setFont(new Font("Arial", Font.BOLD, 22)); // simple configs of text
-        window.add(totalText, gbc);
+        // Image configs
+        int sizePizza = 250;
+        ImageIcon rawIcon = new ImageIcon("assets/pizza1.png"); // load the image
 
-        int largura = 250;
-        int altura = 250;
+        // Ajust the sze
+        Image img = rawIcon.getImage().getScaledInstance(sizePizza, sizePizza, Image.SCALE_SMOOTH); 
+        ImageIcon iconPizza = new ImageIcon(img); // create the final image of pizza ajusted
 
-        ImageIcon rawIcon = new ImageIcon("assets/pizza1.png"); //  load the original image
-        Image img = rawIcon.getImage().getScaledInstance(largura, altura, Image.SCALE_SMOOTH); // configure the image to create the icon
-        ImageIcon iconPizza = new ImageIcon(img); // create the icon of pizza's button
+        // pizza button 
+        JButton pizza = new JButton(iconPizza); 
+        pizza.setContentAreaFilled(false); // background transparent
+        pizza.setBorderPainted(false);     // remove the border
+        pizza.setFocusPainted(false);      // remove the select box on hover 
 
-        JButton pizza = new JButton(iconPizza); // create the button/clicker
-        pizza.setPreferredSize(new Dimension(largura, altura)); // set the size of button
-        pizza.setContentAreaFilled(false); // set the area of button
-        pizza.setBorderPainted(false);
-        pizza.setFocusPainted(false);
-        pizza.setOpaque(false);
-
-        pizza.addActionListener(e -> { // when click on pizza button
-            numPizzas++; // add in num of pizzas
-            totalText.setText("Total: " + numPizzas); //update the text
+        // click action
+        pizza.addActionListener(e -> { 
+            numPizzas++; // plus 1 in var
+            totalText.setText("Total: " + numPizzas); // update the text
         });
 
-        gbc.gridy = 1;
-        window.add(pizza, gbc);
+        // add in window
+        
+        window.add(totalText, BorderLayout.NORTH); // set the text in north (top)
+        window.add(pizza, BorderLayout.CENTER);    // set the button in center
+        // I think this way is more easy to understand for every (java)programmer when see my code, but this project is a simple test when I create my classes of Git in OnlyOffice
+        // It is the way how I learn programming... I write slides and documents about how Im going to teach a class
 
+        // show the window
         window.setVisible(true);
     }
 }
